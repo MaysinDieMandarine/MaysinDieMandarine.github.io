d8c1807613cabae5e6dcf9fcf5080be4eaaeeeeb
Maysin <163896913+MaysinDieMandarine@users.noreply.github.com>
2024-04-04 22:23:22 +0200

    Add files via upload

---
+++ dev-channel.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Dev-Channel</title>
+		<script src="script.js"/>
+		<link rel="stylesheet" href="stylesheet.css">
+	</head>
+	<body>
+	Hier ist ein Java-Codebeispiel:<br/>
+	<div class="code">
+	<pre><code>
+	public class HalloWelt
+	{
+	  public static void main(String[] args)
+	  {
+	    System.out.println("Hallo Welt");
+	  }
+	}
+	</code></pre>
+	<button onclick="helloworldexample();">Ergebnis(in JavaScript nachgebildet)</button>
+	<p>Irgendein Thema? Send es mir als <a href="mailto:msa.scratchcat@gmail.com">Email</a>(nur Deutsch und Englisch)</p>
+	</body>
+</html>
